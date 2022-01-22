ab209bf34cd1091883dce71962a4ee92aa3f367a
Ester Bruden <ester.bruden@gmail.com>
2022-01-21 18:51:15 -0800

    Initial commit -Redo

---
+++ src/main/java/com/myplantdiary/enterprise/EnterpriseApplicationTests.java
@@ -0,0 +1,11 @@
+package com.myplantdiary.enterprise;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+public class EnterpriseApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
