97fc8ead942fd1120c8afd8a9f2b5fb986b96688
Ester Bruden <ester.bruden@gmail.com>
2022-03-10 17:08:31 -0800

    17- Test Driven Design and BDD: Hands on example in IntelliJ IDEA

--- src/main/java/com/myplantdiary/enterprise/EnterpriseApplicationTests.java
+++ src/main/java/com/myplantdiary/enterprise/EnterpriseApplicationTests.java
@@ -1,11 +1,44 @@
 package com.myplantdiary.enterprise;
 
+import com.myplantdiary.enterprise.dto.Specimen;
+import com.myplantdiary.enterprise.service.ISpecimenService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testng.annotations.Test;
 
+import static org.testng.AssertJUnit.assertEquals;
+
 @SpringBootTest
 public class EnterpriseApplicationTests {
+@Autowired
+    private ISpecimenService specimenService;
+    private Specimen specimen = new Specimen();
+
     @Test
     void contextLoads() {
     }
+
+    @Test
+    void fetchSpecimenByID_returnsRedbudForID83() throws Exception  {
+        givenSpecimenDataAreAvailable();
+        whenSearchSpecimenWithID83();
+        thenReturnOneEasternRedbudSpecimenForID83();
+    }
+
+    private void givenSpecimenDataAreAvailable() throws Exception {
+        /*
+        Mockito.when(specimenDAO.save(specimen)).thenReturn(specimen);
+        specimenService = new SpecimenServiceStub(specimenDAO);
+         */
+    }
+
+    private void whenSearchSpecimenWithID83() {
+        specimen = specimenService.fetchById(83);
+    }
+
+    private void thenReturnOneEasternRedbudSpecimenForID83() {
+        String description = specimen.getDescription();
+        assertEquals("Eastern Redbud", description);
+    }
+
 }
---
+++ src/main/java/com/myplantdiary/enterprise/service/SpecimenServiceStub.java
@@ -0,0 +1,36 @@
+package com.myplantdiary.enterprise.service;
+
+import com.myplantdiary.enterprise.dto.Specimen;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpecimenServiceStub implements ISpecimenService{
+    /*
+    private ISpecimenDAO specimenDAO;
+*/
+    public SpecimenServiceStub() {
+
+    }
+/*
+    public SpecimenServiceStub(ISpecimenDAO specimenDAO) {
+
+        this.specimenDAO = specimenDAO;
+    }
+*/
+    @Override
+    public Specimen fetchById(int id) {
+        // hardcoded representation of specimen
+        Specimen specimen = new Specimen();
+        specimen.setDescription("Eastern Redbud");
+        specimen.setSpecimenId("83");
+        return specimen;
+    }
+
+    @Override
+    public Specimen save(Specimen specimen) throws Exception {
+        /*
+        return specimenDAO.save(specimen);
+         */
+        return specimen;
+    }
+}
